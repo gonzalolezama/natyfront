51dc5306f4e55de57da7e4dd065827b32ec65f8a
Gonzalo Lezama <gonzalo@lezamapc.com.ar>
2024-11-26 16:38:53 -0300

    carrusel3

--- src/paginas/Home.js
+++ src/paginas/Home.js
@@ -1,26 +1,27 @@
-import './../styles/home.css'
+import './../styles/home.css';
+
 const Home = (props) => {
     return (
         <main className="holder">
-            <div id="carouselExample" class="carousel slide">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="img/home/img01.jpg" class="d-block w-100" alt="casa 1" />
+            <div id="carouselExample" className="carousel slide" data-bs-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src="/img/home/img01.jpg" className="d-block w-100" alt="casa 1" />
                     </div>
-                    <div class="carousel-item">
-                        <img src="img/home/img02.jpg" class="d-block w-100" alt="casa 2" />
+                    <div className="carousel-item">
+                        <img src="/img/home/img02.jpg" className="d-block w-100" alt="casa 2" />
                     </div>
-                    <div class="carousel-item">
-                        <img src="img/home/img03.jpg" class="d-block w-100" alt="casa 3" />
+                    <div className="carousel-item">
+                        <img src="/img/home/img03.jpg" className="d-block w-100" alt="casa 3" />
                     </div>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
                 </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
                 </button>
             </div>
             <div className="columnas">
