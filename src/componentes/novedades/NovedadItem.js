6cce74158f6e63989f45050e030613d758ad5a74
Gonzalo Lezama <gonzalo@lezamapc.com.ar>
2024-11-27 00:40:16 -0300

    7

--- src/componentes/novedades/NovedadItem.js
+++ src/componentes/novedades/NovedadItem.js
@@ -1,16 +1,18 @@
 import React from "react";
 
 const NovedadItem = (props) => {
-    const{ title, subtitle, imagen, body } = props;
+    const { title, subtitle, imagen, body } = props;
 
     return (
         <div className="novedades">
             <h4>{title}</h4>
             <p> {subtitle} </p>
+            <div className="novedades2"></div>
             <img src={imagen} alt={title} />
-            <div dangerouslySetInnerHTML={{__html:body}} />
+            <div dangerouslySetInnerHTML={{ __html: body }} />
             <hr />
         </div>
+        </div >
     
     );
 }
