7d3021033d82bbcfd7b8424a41a830153deba170
Gonzalo Lezama <gonzalo@lezamapc.com.ar>
2024-11-27 02:53:48 -0300

    vener5

--- src/paginas/Vender.js
+++ src/paginas/Vender.js
@@ -1,43 +1,43 @@
 const Vender = (props) => {
     return (
         <main className="holder">
-        <h2>Vender una Propiedad con Nosotros</h2>
-        <p>Vender una propiedad en el mercado de bienes raíces (RMX) puede ofrecer varios beneficios. Aquí te dejo
-            algunos de los más destacados:</p>
-        <div className="vender">
-            <img src="img/servicios/vender.jpg" alt="vender"/>
-            <div className="info">
-                <h4>Liquidez inmediata:</h4>
-                <p>Vender una propiedad puede generar una gran suma de dinero en efectivo que puedes utilizar para otros
-                    propósitos, como invertir en otras propiedades, pagar deudas, o financiar nuevos proyectos.</p>
+            <h2>Vender una Propiedad con Nosotros</h2>
+            <p>Vender una propiedad en el mercado de bienes raíces (RMX) puede ofrecer varios beneficios. Aquí te dejo
+                algunos de los más destacados:</p>
+            <div className="vender">
+                <div className="info">
+                    <h4>Liquidez inmediata:</h4>
+                    <p>Vender una propiedad puede generar una gran suma de dinero en efectivo que puedes utilizar para otros
+                        propósitos, como invertir en otras propiedades, pagar deudas, o financiar nuevos proyectos.</p>
+                    <img src="img/servicios/vender.jpg" alt="vender" />
+                </div>
             </div>
-        </div>
-        <div className="vender">
-            <img src="img/servicios/vender2.jpg" alt="vender1"/>
-            <div className="info">
-                <h4>Valor de mercado:</h4>
-                <p>Si has mantenido bien tu propiedad y el mercado está en alza, puedes obtener un precio excelente que
-                    refleje el valor real del inmueble.</p>
+            <div className="vender">
+                <div className="info">
+                    <h4>Valor de mercado:</h4>
+                    <p>Si has mantenido bien tu propiedad y el mercado está en alza, puedes obtener un precio excelente que
+                        refleje el valor real del inmueble.</p>
+                    <img src="img/servicios/vender2.jpg" alt="vender1" />
+                </div>
             </div>
-        </div>
-        <div className="vender">
-            <img src="img/servicios/vender3.jpg" alt="vender2"/>
-            <div class="info">
-                <h4>Menos responsabilidad:</h4>
-                <p>Al vender tu propiedad, te liberas de las responsabilidades que conlleva ser propietario, como el
-                    mantenimiento, los impuestos y las reparaciones.</p>
+            <div className="vender">
+                <div class="info">
+                    <h4>Menos responsabilidad:</h4>
+                    <p>Al vender tu propiedad, te liberas de las responsabilidades que conlleva ser propietario, como el
+                        mantenimiento, los impuestos y las reparaciones.</p>
+                    <img src="img/servicios/vender3.jpg" alt="vender2" />
+                </div>
             </div>
-        </div>
-        <div className="vender">
-            <img src="img/servicios/vender4.jpg" alt="vender3"/>
-            <div className="info">
-                <h4>Diversificación de inversiones: </h4>
-                <p>Al vender, puedes diversificar tus inversiones, invirtiendo en diferentes tipos de activos que
-                    podrían ofrecer mejores rendimientos.
-                </p>
+            <div className="vender">
+                <div className="info">
+                    <h4>Diversificación de inversiones: </h4>
+                    <p>Al vender, puedes diversificar tus inversiones, invirtiendo en diferentes tipos de activos que
+                        podrían ofrecer mejores rendimientos.
+                    </p>
+                    <img src="img/servicios/vender4.jpg" alt="vender3" />
+                </div>
             </div>
-        </div>
-    </main>
+        </main>
     )
 }
 
